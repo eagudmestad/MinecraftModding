ce4fe5a3b15fc876c70c35c838c0116a62a4d2df
Evan Gudmestad <eagudmestad@ranken.edu>
2021-07-07 11:49:57 -0500

    Special Item

--- src/main/java/com/example/examplemod/core/init/ItemInit.java
+++ src/main/java/com/example/examplemod/core/init/ItemInit.java
@@ -1,6 +1,7 @@
 package com.example.examplemod.core.init;
 
 import com.example.examplemod.ExampleMod;
+import com.example.examplemod.common.items.SpecialItem;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -15,7 +16,12 @@ public class ItemInit {
     public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item",
             () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
 
+    public static final RegistryObject<SpecialItem> SPECIAL_ITEM = ITEMS.register("special_item",
+            () -> new SpecialItem(new Item.Properties().group(ItemGroup.MISC)));
+
     //Block Items
     public static final RegistryObject<BlockItem> EXAMPLE_BLOCK = ITEMS.register("example_block",
             () -> new BlockItem(BlockInit.EXAMPLE_BLOCK.get(),new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)));
+
+
 }
