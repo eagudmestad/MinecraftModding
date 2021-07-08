844cd537e764a0e2e07f77029117690d6d6573b8
Evan Gudmestad <eagudmestad@ranken.edu>
2021-07-08 13:34:47 -0500

    recipes & Special Item Assets

--- src/main/java/com/example/examplemod/core/init/ItemInit.java
+++ src/main/java/com/example/examplemod/core/init/ItemInit.java
@@ -2,10 +2,9 @@ package com.example.examplemod.core.init;
 
 import com.example.examplemod.ExampleMod;
 import com.example.examplemod.common.items.SpecialItem;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.Items;
+import net.minecraft.item.*;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -18,6 +17,12 @@ public class ItemInit {
 
     public static final RegistryObject<SpecialItem> SPECIAL_ITEM = ITEMS.register("special_item",
             () -> new SpecialItem(new Item.Properties().group(ExampleMod.TUTORIAL_GROUP)));
+
+
+    public static final RegistryObject<Item> EXAMPLE_FOOD = ITEMS.register("example_food",
+            () -> new Item(new Item.Properties().group(ExampleMod.TUTORIAL_GROUP)
+                    .food(FoodInit.EXAMPLE_FOOD)));
+
 
     //Block Items
     public static final RegistryObject<BlockItem> EXAMPLE_BLOCK = ITEMS.register("example_block",
